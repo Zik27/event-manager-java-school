5d3f34b624a0bd06435935d6eb1e48c3957c1c03
avomika <jane_muse@mail.ru>
2021-09-04 19:47:21 +0300

    test

--- bot-server/src/main/java/ru/dbtc/bot/Bot.java
+++ bot-server/src/main/java/ru/dbtc/bot/Bot.java
@@ -1,13 +1,22 @@
 package ru.dbtc.bot;
 
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-
+@PropertySource("classpath:app.properties")
+@Component
 public class Bot extends TelegramLongPollingBot {
-    public static final String TOKEN = "1809944157:AAEN5NXfJ5UanwZtwYqUNqXFOFKImkUWolE";
-    public static final String USERNAME = "JavaEventManagerBot";
+    @Value("${bot.botToken}")
+    private String TOKEN = "1809944157:AAEN5NXfJ5UanwZtwYqUNqXFOFKImkUWolE";
+
+    @Value("${bot.userName}")
+    private String USERNAME = "JavaEventManagerBot";
 
     @Override
     public String getBotUsername() {
@@ -28,7 +37,7 @@ public class Bot extends TelegramLongPollingBot {
             message.setText(update.getMessage().getText());
             execute(message);
             SendMessage sendMessage = new SendMessage();
-            sendMessage.setText("KAK DELA?");
+            sendMessage.setText("wrrrr?");
             sendMessage.setChatId(update.getMessage().getChatId().toString());
             execute(sendMessage); // Call method to send the message
         }
