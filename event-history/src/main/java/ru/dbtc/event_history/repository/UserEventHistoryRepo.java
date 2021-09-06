85bd54af5ad5553a60a530ea44b3e02e614a6a9d
Elena <batichev22@gmail.com>
2021-09-07 02:07:53 +0300

    change controller and serviceImpl

--- event-history/src/main/java/ru/dbtc/event_history/repository/UserEventHistoryRepo.java
+++ event-history/src/main/java/ru/dbtc/event_history/repository/UserEventHistoryRepo.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.dbtc.event_history.model.UserEventHistory;
 
+import java.util.Optional;
+
 @Repository
 public interface UserEventHistoryRepo extends JpaRepository<UserEventHistory, Integer> {
+    Optional<UserEventHistory> findByUserIdAndEventId(String userId, int eventId);
+    void deleteByUserIdAndEventId(String userId, int eventId);
 }
